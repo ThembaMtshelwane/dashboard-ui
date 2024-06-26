767bb324d733f3174f6ad549b6a6b65fc7a3c03b
Themba Mtshelwane <Thembamm3@gmail.com>
2024-06-26 15:53:34 +0200

    Main ui

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,10 @@
+import styles from './card.module.css'
+
+const Card = () => {
+  return (
+    <section className={styles.cardContainer}>
+      <a href="https://www.google.com/">Card</a>
+    </section>
+  )
+}
+export default Card
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,18 @@
+import Card from '../Card/Card'
+import styles from './main.module.css'
+
+const Main = () => {
+  return (
+    <section className={styles.mainContainer}>
+      <Card />
+      <Card />
+      <Card /> <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </section>
+  )
+}
+export default Main
